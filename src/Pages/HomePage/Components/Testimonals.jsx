683e5859ae2b806b11b0277e2724d447d8520edf
smile995 <ah.smile995@gmail.com>
2024-12-25 23:14:01 +0600

    add user rating section

--- src/Pages/HomePage/Components/Testimonals.jsx
+++ src/Pages/HomePage/Components/Testimonals.jsx
@@ -1,14 +1,70 @@
-import SectionTitle from "../../../SharedComponents/SectionTitle/SectionTitle"
-
+import SectionTitle from "../../../SharedComponents/SectionTitle/SectionTitle";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Rating } from "@smastrom/react-rating";
+import icon from '../../../assets/icon/quote.png'
+import "@smastrom/react-rating/style.css";
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 const Testimonals = () => {
-  return (
-    <div>
-        <div>
-            <SectionTitle heading={"Testimonials"} subheading={'What Our Clinets Says'}></SectionTitle>
-        </div>
-    </div>
-  )
-}
+  const [resviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  console.log(resviews);
 
-export default Testimonals
+  return (
+    <div className="my-10">
+      <div>
+        <SectionTitle
+          heading={"Testimonials"}
+          subheading={"What Our Clinets Says"}
+        ></SectionTitle>
+      </div>
+      <div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {resviews.map((review) => (
+            <SwiperSlide key={review._id} className="my-10">
+              <div className="text-center w-3/4 mx-auto space-y-2">
+                <div className="flex justify-center">
+                  <Rating
+                    style={{ maxWidth: 180 }}
+                    value={review.rating}
+                    readOnly
+                  />
+                </div>
+                <div className="flex justify-center">
+                    <img src={icon} alt="" />
+                </div>
+                <p>{review.details}</p>
+                <p className="text-[#CD9003] font-semibold text-xl">
+                  {review.name}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonals;
