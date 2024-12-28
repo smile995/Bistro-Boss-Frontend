0e957410697af7bb143938965ca27b153250a90f
smile995 <ah.smile995@gmail.com>
2024-12-28 20:41:30 +0600

    Finish the work at manu page where everything is dynamic  and reusable

---
+++ src/Pages/OurManu/Components/CategorizedFood.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom"
+import ItemViewer from "../../../SharedComponents/ItemViewer/ItemViewer"
+
+const CategorizedFood = ({items}) => {
+  return (
+    <div className="mb-10">
+        
+        <div className=" grid md:grid-cols-2 gap-5 md:gap-10 p-3">
+        {items.map((manu) => (
+          <ItemViewer key={manu._id} manu={manu} />
+        ))}
+      </div>
+      <div className="flex justify-center mt-2">
+        <Link to={'our-manu'}>
+        <button className="px-5  py-3 font-semibold border-b-black  border-b-4 rounded-lg hover:text-[#BB8506] shadow-xl ">
+        ORDER YOUR FAVOURITE FOOD
+        </button></Link>
+      </div>
+    </div>
+  )
+}
+
+export default CategorizedFood
--- src/Pages/OurManu/OurManu.jsx
+++ src/Pages/OurManu/OurManu.jsx
@@ -1,6 +1,12 @@
 import useManu from "../../Hooks/useManu";
 import ItemCover from "../../SharedComponents/ItemCover/ItemCover";
+import SectionTitle from "../../SharedComponents/SectionTitle/SectionTitle";
+import CategorizedFood from "./Components/CategorizedFood";
 import banner from "../../assets/menu/banner3.jpg";
+import dessert from "../../assets/menu/dessert-bg.jpeg";
+import pizza from "../../assets/menu/pizza-bg.jpg";
+import salad from "../../assets/menu/salad-bg.jpg";
+import soup from "../../assets/menu/soup-bg.jpg";
 
 const OurManu = () => {
   const [manus] = useManu();
@@ -16,7 +22,45 @@ const OurManu = () => {
         tag={"Would you like to try a dish?"}
         img={banner}
       ></ItemCover>
-      <div></div>
+      <div>
+        <SectionTitle
+          heading="Today's Offers"
+          subheading="Don't Miss"
+        ></SectionTitle>
+        <CategorizedFood items={offereds}></CategorizedFood>
+      </div>
+      <div>
+        <ItemCover
+          img={dessert}
+          title="desserts"
+          descrition="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
+        <CategorizedFood items={desserts} />
+      </div>
+      <div>
+        <ItemCover
+          img={pizza}
+          title="pizzas"
+          descrition="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
+        <CategorizedFood items={pizzas} />
+      </div>
+      <div>
+        <ItemCover
+          img={salad}
+          title="salads"
+          descrition="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
+        <CategorizedFood items={salads} />
+      </div>
+      <div>
+        <ItemCover
+          img={soup}
+          title="soups"
+          descrition="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        />
+        <CategorizedFood items={soups} />
+      </div>
     </div>
   );
 };
