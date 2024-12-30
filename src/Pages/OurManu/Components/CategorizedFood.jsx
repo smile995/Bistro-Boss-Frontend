a665beaceb3a01cb9ae829f0e2ad545ecb0f8fa8
smile995 <ah.smile995@gmail.com>
2024-12-30 19:21:00 +0600

    make something change

--- src/Pages/OurManu/Components/CategorizedFood.jsx
+++ src/Pages/OurManu/Components/CategorizedFood.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom"
 import ItemViewer from "../../../SharedComponents/ItemViewer/ItemViewer"
 
 const CategorizedFood = ({items}) => {
+  console.log(items);
+  
   return (
     <div className="mb-10">
         
--- src/SharedComponents/FoodCard/FoodCard.jsx
+++ src/SharedComponents/FoodCard/FoodCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 const FoodCard = ({ food }) => {
   const { name, image, price, recipe } = food;
@@ -7,15 +9,19 @@ const FoodCard = ({ food }) => {
         <figure>
           <img className=" rounded-t w-full " src={image} alt="Shoes" />
         </figure>
-        <p className="bg-[#e8e8e8] text-[#BB8506] border-2  px-3 py-1 font-semibold rounded absolute top-4 right-4 text-xl">${price}</p>
+        <p className="bg-[#e8e8e8] text-[#BB8506] border-2  px-3 py-1 font-semibold rounded absolute top-4 right-4 text-xl">
+          ${price}
+        </p>
       </div>
       <div className="card-body text-center">
         <h2 className="text-2xl font-semibold">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-center">
-          <button className="text-[#BB8506] uppercase bg-[#e8e8e8] rounded py-3 px-4 font-semibold border-b-2 border-[#BB8506] hover:bg-[#1F2937]">
-            add to cart
-          </button>
+          <Link >
+            <button className="text-[#BB8506] uppercase bg-[#e8e8e8] rounded py-3 px-4 font-semibold border-b-2 border-[#BB8506] hover:bg-[#1F2937]">
+              add to cart
+            </button>
+          </Link>
         </div>
       </div>
     </div>
