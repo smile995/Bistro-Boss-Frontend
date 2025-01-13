7cb109b1e3d1cecbbc5fced440f0a2f869e5cd22
smile995 <ah.smile995@gmail.com>
2025-01-13 22:34:35 +0600

    make some changes in signin page

--- src/Pages/Signin/SignIn.jsx
+++ src/Pages/Signin/SignIn.jsx
@@ -3,7 +3,7 @@ import image from "../../assets/others/authentication2.png";
 const SignIn = () => {
   return (
     <div
-      className="hero md:min-h-screen md:flex lg:gap-20 md:gap-10 items-center md:p-10 p-5"
+      className="min-h-screen md:flex  md:gap-10 items-center md:p-10 p-5"
       style={{
         backgroundImage: `url(${woodBg})`, // Use the imported image
         backgroundSize: "cover", // Optional: Cover the entire container
@@ -11,53 +11,60 @@ const SignIn = () => {
         backgroundRepeat: "no-repeat", // Optional: Prevent repeating
       }}
     >
-      <div className="max-w-1/2">
+      <div className="md:max-w-1/2">
         <img className="w-full" src={image} alt="image missing" />
       </div>
-      <div className="md:max-w-1/2">
+      <div className="flex-1">
         <form>
-        
-            <div className="form-control ">
-              <label className="label">
-                <span className="label-text">Name*</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Your Name"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control ">
-              <label className="label">
-                <span className="label-text">Email*</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Example@gmail.com"
-                className="input input-bordered"
-                required
-              />
-            </div>
-         
-          <div className="form-control">
+          <h3 className="md:text-4xl text-2xl font-bold text-center">Login</h3>
+
+          <div className="form-control ">
             <label className="label">
-              <span className="label-text">Phone*</span>
+              <span className="label-text">Email*</span>
             </label>
             <input
-              type="text"
-              placeholder="Your Phone Number"
+              type="email"
+              name="email"
+              placeholder="Example@gmail.com"
               className="input input-bordered"
               required
             />
           </div>
-         
-       
-          <div className="mt-6 flex items-center justify-center">
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Password*</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Type your password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div>
+            <h1>react capche will comes here</h1>
+          </div>
+
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Captcha*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type the Captcha here"
+              name="captcha"
+              className="input input-bordered"
+              required
+            />
+            
+          </div>
+
+          <div className="mt-6">
             <button
               type="submit"
-              className="px-5 py-3 rounded text-white font-semibold bg-gradient-to-r from-[#835D23] to-[#B58130] flex items-center gap-2 "
-            > Send Message  </button>
+              className="px-5 py-3 rounded text-white font-semibold bg-[#dbb884]  w-full  "
+            > Send Message</button>
           </div>
         </form>
       </div>
