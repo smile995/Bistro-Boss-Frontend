abf3919e7a31f26db5c7015bdb1b1d6a51f0ef7f
smile995 <ah.smile995@gmail.com>
2025-01-13 23:25:02 +0600

    Make a simple functionality on password feild

--- src/Pages/Signin/SignIn.jsx
+++ src/Pages/Signin/SignIn.jsx
@@ -1,21 +1,37 @@
 import woodBg from "../../assets/reservation/wood-grain-pattern-gray1x.png";
 import image from "../../assets/others/authentication2.png";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { useState } from "react";
 const SignIn = () => {
+  const [seen, setSeen] = useState(false);
+  const [eye, setEye] = useState(false);
+  const handleSeenPassword = () => {
+    setSeen(!seen);
+    setEye(!eye)
+  };
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const captcha = form.captcha.value;
+    console.log(email, password, captcha);
+  };
   return (
     <div
       className="min-h-screen md:flex  md:gap-10 items-center md:p-10 p-5"
       style={{
-        backgroundImage: `url(${woodBg})`, // Use the imported image
-        backgroundSize: "cover", // Optional: Cover the entire container
-        backgroundPosition: "center", // Optional: Center the image
-        backgroundRepeat: "no-repeat", // Optional: Prevent repeating
+        backgroundImage: `url(${woodBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
       }}
     >
       <div className="md:max-w-1/2">
         <img className="w-full" src={image} alt="image missing" />
       </div>
       <div className="flex-1">
-        <form>
+        <form onSubmit={handleLogin}>
           <h3 className="md:text-4xl text-2xl font-bold text-center">Login</h3>
 
           <div className="form-control ">
@@ -30,41 +46,56 @@ const SignIn = () => {
               required
             />
           </div>
-          <div className="form-control ">
-            <label className="label">
-              <span className="label-text">Password*</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Type your password"
-              className="input input-bordered"
-              required
-            />
+          <div className="flex items-end">
+            <div className="form-control flex-1 ">
+              <label className="label">
+                <span className="label-text">Password*</span>
+              </label>
+              <input
+                type={seen ? "text" : "password"}
+                placeholder="Type your password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div>
+              <button
+                onClick={handleSeenPassword}
+                className="-ml-14 text-black text-2xl p-3"
+              >
+                {eye ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
           </div>
           <div>
             <h1>react capche will comes here</h1>
           </div>
 
-          <div className="form-control ">
-            <label className="label">
-              <span className="label-text">Captcha*</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Type the Captcha here"
-              name="captcha"
-              className="input input-bordered"
-              required
-            />
-            
+          <div className="flex justify-between items-end  gap-2">
+            <div className="form-control flex-1 ">
+              <label className="label">
+                <span className="label-text">Captcha*</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type the Captcha here"
+                name="captcha"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div>
+              <button className="btn btn-secondary">Valided</button>
+            </div>
           </div>
 
           <div className="mt-6">
-            <button
+            <input
+              className="px-5 py-3 w-full rounded text-white font-semibold bg-[#dbb884]"
               type="submit"
-              className="px-5 py-3 rounded text-white font-semibold bg-[#dbb884]  w-full  "
-            > Send Message</button>
+              value="Login"
+            />
           </div>
         </form>
       </div>
