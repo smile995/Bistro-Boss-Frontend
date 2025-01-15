3e22509b32b4e98f82c6a890e51b63820fd32604
smile995 <ah.smile995@gmail.com>
2025-01-15 10:19:02 +0600

    Making the functionality to login with email and github as social login

--- src/SharedComponents/SocialLogin/SocialLogin.jsx
+++ src/SharedComponents/SocialLogin/SocialLogin.jsx
@@ -1,19 +1,70 @@
 import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa6";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../../Firebase/Firebase.config";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  const notify = () => toast("You are logged in successfully");
+  const provider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
+  const signinwithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        if (user.email) {
+          notify();
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+
+        console.log(error.message);
+      });
+  };
+
+  const signinwithGithub = () => {
+    signInWithPopup(auth, gitProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        if (user.email) {
+          notify();
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <div>
       <h3 className="text-center font-semibold">Or Sign in with</h3>
       <div className="flex justify-center items-center mt-3 gap-8">
-        <button className="btn btn-circle btn-outline">
+        <button
+          onClick={signinwithGoogle}
+          className="btn btn-circle btn-outline"
+        >
           <FaGoogle className="text-2xl" />
         </button>
-        <button className="btn btn-circle btn-outline">
+        <button
+          onClick={signinwithGithub}
+          className="btn btn-circle btn-outline"
+        >
           <FaGithub className="text-2xl" />
         </button>
         <button className="btn btn-circle btn-outline">
           <FaFacebookF className="text-2xl" />
         </button>
+      </div>
+      <div>
+        <ToastContainer />
       </div>
     </div>
   );
