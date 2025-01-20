b19df28d87dffcf65943a61c7e7189bd09bb7fed
smile995 <ah.smile995@gmail.com>
2025-01-20 22:57:55 +0600

    Make the form to get the info about food to post in the database

--- src/Pages/AddItems/AddItem.jsx
+++ src/Pages/AddItems/AddItem.jsx
@@ -11,30 +11,59 @@ const AddItem = () => {
           <form className="card-body">
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Email</span>
+                <span className="label-text">Recipe Name*</span>
               </label>
               <input
-                type="email"
-                placeholder="email"
+                type="text"
+                placeholder="Food Name..."
+                name="name"
                 className="input input-bordered"
                 required
               />
             </div>
+            <div className="md:flex gap-5 items-center">
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">Category*</span>
+                </div>
+                <select className="select select-bordered">
+                  <option disabled selected>
+                    Select Category
+                  </option>
+                  <option>Pizza</option>
+                  <option>Salad</option>
+                  <option>Drinks</option>
+                  <option>Soup</option>
+                  <option>Dessert</option>
+                </select>
+              </label>
+              <div className="form-control flex-1">
+                <label className="label">
+                  <span className="label-text">Price*</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Price"
+                  name="price"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+            </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Password</span>
+                <span className="label-text">Recipe Details</span>
               </label>
+              <textarea
+                className="textarea textarea-bordered"
+                placeholder="Write details abot food"
+              ></textarea>
+            </div>
+            <div>
               <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
+                type="file"
+                className="file-input file-input-bordered file-input-accent w-full max-w-xs"
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
