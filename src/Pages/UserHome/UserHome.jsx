78882cac3bd3ad5d3b6820f6cd0be5a6a9431cbb
smile995 <ah.smile995@gmail.com>
2025-01-28 11:59:26 +0600

    working on user home page

--- src/Pages/UserHome/UserHome.jsx
+++ src/Pages/UserHome/UserHome.jsx
@@ -1,9 +1,65 @@
-
+import { useState } from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import useBistro from "../../Hooks/useBistro";
+import { FaBookOpen, FaMoneyBill, FaUtensils } from "react-icons/fa6";
 
 const UserHome = () => {
-  return (
-    <div>UserHome</div>
-  )
-}
+  const { user } = useBistro();
+  const axiosSecure = useAxiosSecure();
+  const [reviews, setReviews] = useState(0);
+  const [orders, setOrder] = useState(0);
+  const [foods, setFood] = useState(0);
+  axiosSecure.get(`/user-state/${user?.email}`).then((res) => {
+    setFood(res?.data?.menus);
+    setReviews(res?.data?.reviews);
+    setOrder(res?.data?.orders);
+  });
+  console.log(reviews, orders, foods);
 
-export default UserHome
+  return (
+    <div>
+      <h2 className="text-3xl font-bold">
+        <span> Hi, Welcome</span> {user ? user?.displayName : "Back !"}{" "}
+      </h2>
+      <div>
+        <div className="stats shadow w-full my-10">
+          <div className="stat bg-gradient-to-l from-[#FCDBFF] to-[#BB34F5]">
+            <div className="stat-figure text-secondary">
+              <FaUtensils className="text-4xl" />
+            </div>
+            <div className="stat-title">Menus</div>
+            <div className="stat-value">{foods}</div>
+          </div>
+
+          <div className="stat bg-gradient-to-l from-[#FDE8C0] to-[#D3A256] ">
+            <div className="stat-figure text-secondary">
+              <FaBookOpen className="text-4xl" />
+            </div>
+            <div className="stat-title">Orders</div>
+            <div className="stat-value">{orders}</div>
+          </div>
+
+          <div className="stat bg-gradient-to-l from-[#FECDE9] to-[#FE4880] ">
+            <div className="stat-figure text-secondary">
+              <FaMoneyBill className="text-4xl" />
+            </div>
+            <div className="stat-title">Payments</div>
+            <div className="stat-value">{orders}</div>
+          </div>
+        </div>
+      </div>
+      <div className="md:flex w-full rounded">
+        <div className="md:w-1/2 w-full md:p-10 p-5 bg-red-500 flex items-center justify-center border-r-4 border-[#D3A256]">
+          <div className="avatar">
+            <div className="mask mask-hexagon w-24">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+        </div>
+        <div className="md:w-1/2 w-full md:p-10 p-5 bg-red-100"></div>
+      </div>
+    </div>
+  );
+};
+
+export default UserHome;
