4cfea3954b507975fb7b551cc253dde4ef0ba166
smile995 <ah.smile995@gmail.com>
2025-01-28 13:40:43 +0600

    User payment history page is ready in frontend and backend

--- src/Pages/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/PaymentHistory/PaymentHistory.jsx
@@ -1,9 +1,51 @@
-
+import { useState } from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import useBistro from "../../Hooks/useBistro";
+import SectionTitle from "../../SharedComponents/SectionTitle/SectionTitle";
 
 const PaymentHistory = () => {
-  return (
-    <div>PaymentHistory</div>
-  )
-}
+  const { user } = useBistro();
+  const axiosSecure = useAxiosSecure();
+  const [payments, setPayment] = useState([]);
+  axiosSecure.get(`/payments/${user.email}`).then((res) => {
+    setPayment(res.data);
+  });
 
-export default PaymentHistory
+  return (
+    <div>
+      <SectionTitle subheading={"At a Glance!"} heading={"Payments History"} />
+      <div>
+        <h1 className="text-3xl font-semibold">Total Payments:  {payments?.length}</h1>
+      </div>
+      <div className="my-4">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-[#D1A054]">
+              <tr>
+                <th>#</th>
+                <th>Email</th>
+                <th>Transsaction_ID</th>
+                <th>Status</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {payments?.map((payment,idx) => (
+                <tr key={payment._id}>
+                  <th>{idx+1}</th>
+                  <td>{payment?.email}</td>
+                  <td>{payment?.transactionId}</td>
+                  <td>{payment?.status}</td>
+                  <td>{payment?.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentHistory;
